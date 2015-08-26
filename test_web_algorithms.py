9ce43a2aafeb69ac11ef068488596075b580b28e
cc_chs <computerclub.chs@gmail.com>
2015-08-26 15:58:31 +0000

    modified test_web_algorithms

--- test_web_algorithms.py
+++ test_web_algorithms.py
@@ -13,12 +13,12 @@ import sys
 choices = ['RANDOMST','EXTREMEST']
 choiceIdx = 1
 g = graph.Graph()
-g.read_graph('graphs/planar_dir_5.txt')
+# g.read_graph('graphs/planar_dir_5.txt')
 ''' Generate a directed graph '''
-# g.generate_planar_graph(30,0.5,True)
-# g.save_graph('graphs/planar_dir_30.txt')
+g.generate_planar_graph(30,0.5,True)
+g.save_graph('graphs/planar_dir_30.txt')
 walk_anim = web_algorithms.random_walk(g,10000)
 # web_algorithms.in_degrees(g)
 g.draw(None,None,True)
-g.show()
+g.print_graph('results/planar_dir_30.png')
 # granim.animate(g,walk_anim,granim.animate_func_erase,None,'blue',100,True)
